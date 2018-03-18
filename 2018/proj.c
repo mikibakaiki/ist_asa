13b19d32d7427542af953373cf37d5d631fceaac
mikibakaiki <joaomiguelcampos6@gmail.com>
2018-03-18 00:13:32 +0000

    skelleton of the algorithm is laid out

--- 2018/proj.c
+++ 2018/proj.c
@@ -8,7 +8,6 @@ typedef struct node {
     struct node *next;
 } *link;
 
-
 typedef struct edge {
     int v;
     int w;
@@ -17,7 +16,7 @@ typedef struct edge {
 typedef struct graph {
     int numVertices;
     int numEdges;
-    link *list;
+    link *adj;
 } *Graph;
 
 
@@ -35,10 +34,10 @@ Graph GraphInit(int numVertices) {
     Graph G = malloc(sizeof(struct graph));
     G->numVertices = numVertices;
     G->numEdges = 0;
-    G->list = malloc(numVertices*sizeof(link));
+    G->adj = malloc(numVertices*sizeof(link));
 
     for (i = 0; i < numVertices; i++) {
-        G->list[i] = NULL;
+        G->adj[i] = NULL;
     }
 
     return G;
@@ -48,38 +47,71 @@ void GraphInsertEdge(Graph G, Edge e) {
     int vert1 = e.v;
     int vert2 = e.w;
 
-    G->list[vert1] = InsertBegin(vert1, G->list[vert1]);
-    G->list[vert2] = InsertBegin(vert2, G->list[vert2]);
+    G->adj[vert1] = InsertBegin(vert1, G->adj[vert1]);
+    G->adj[vert2] = InsertBegin(vert2, G->adj[vert2]);
 
     G->numEdges++;
 }
 
-static int time = 0;
+static int visited = 0;
 
 void TarjanSCC(Graph G) {
 
     // disc[u] = time;
     // low[u] = time;
-    int visited = 0;
 
     int *stack;
     stack = malloc(sizeof(int) * G->numVertices);
     int i;
 
     for(i = 0; i < G->numVertices; i++) {
-        G->list[i]->discovery = -1;
+        G->adj[i]->discovery = -1;
     }
 
     for(i = 0; i < G->numVertices; i++) {
-        if(G->list[i]->discovery == -1) {
-            TarjanVisit(G->list[i]);
+        if(G->adj[i]->discovery == -1) {
+            TarjanVisit(G, G->adj[i]);
         }
+    }
+}
+
+
+void TarjanVisit(Graph G, link vertex) {
+
+    G->adj[vertex]->discovery = visited;
+    G->adj[vertex]->lowest = visited;
+    visited++;
+
+    /* por o vertice na stack */
+
+    int i;
+    link *x;
+    for(x = G->adj[vertex]; x->next != NULL; x = x->next) {
+        if (x->discovery == -1) {
+            TarjanVisit(G, &x);
+            G->adj[vertex]->lowest = minimum(G->adj[vertex]->lowest, x->lowest);
+        }
+        /*ou se esse vertice estiver na stack*/
+        else if ( ) {
+            G->adj[vertex]->lowest = minimum(G->adj[vertex]->lowest, x->lowest);
+        }
+    }
+
+    /* this is the root of the SCC */
+    if (G->adj[vertex]->discovery == G->adj[vertex]->lowest) {
+        /* retirar os elementos da stack/lista */
+        /* até que G->adj[vertex]->index == ao elemento que saiu da lista */
     }
 }
 
 
 
 
+}
+
+int minimum(int u, int v) {
+    return u > v ? v : u;
+}
 
 
 
