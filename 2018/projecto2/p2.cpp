01c6f864bb9514f2da9d2c556c67fad66bfb9dd2
mikibakaiki <joaomiguelcampos6@gmail.com>
2018-04-29 05:04:50 +0100

    Almost there

--- 2018/projecto2/p2.cpp
+++ 2018/projecto2/p2.cpp
@@ -17,13 +17,33 @@ typedef struct edge {
 
 } Edge;
 
+// class Edge {
+// public:
+//     int u;
+//     int v;
+//     int cap;
+//     int flow;
+//
+//     Edge(){}
+// };
+
 typedef struct pixel {
     int id;
     int i;
     int j;
-    std::list<Edge> edgeList;
+    std::vector<Edge> edgeList;
 
 } Pixel;
+
+// class Pixel {
+// public:
+//     int id;
+//     int i;
+//     int j;
+//     std::list<Edge> edgeList;
+//
+//     Pixel(){}
+// };
 
 /*    VARIAVEIS GLOBAIS     */
 int numPixels;
@@ -45,8 +65,7 @@ void scanInput() {
     scanf("%d %d", &numLinhas, &numColunas);
 
     numPixels = numLinhas * numColunas;
-    head = malloc(sizeof(struct pixel) * (numPixels + 2));
-
+    head = (Pixel *) malloc(sizeof(struct pixel) * (numPixels + 2));
     head[0].id = SOURCE;                // A primeira posicao de head e o vertice S - source
     head[numPixels + 1].id = SINK;      // A ultima posicao de head e o vertice T - sink
 
@@ -55,15 +74,15 @@ void scanInput() {
     // scan for the first matrix - edges from source vertex S to all vertex x
     for(i = 0; i < numLinhas; i++) {
         for(j = 0; j < numColunas; j++) {
-            scanf("%d", sourceCap);
+            scanf("%d", &sourceCap);
+            head[auxCont].id = auxCont - 1;
+            head[auxCont].i = i;
+            head[auxCont].j = j;
             Edge e;
             e.u = SOURCE;
             e.v = head[auxCont].id;
             e.cap = sourceCap;
             head[auxCont].edgeList.push_back(e);
-            head[auxCont].id = auxCont - 1;
-            head[auxCont].i = i;
-            head[auxCont].j = j;
             auxCont++;
             scanf(" ");
         }
@@ -77,12 +96,14 @@ void scanInput() {
     // scan for the second matrix - edges from all vertex x to sink vertex T
     for(i = 0; i < numLinhas; i++) {
         for(j = 0; j < numColunas; j++) {
-            scanf("%d", toSinkCap);
+            scanf("%d", &toSinkCap);
             Edge e;
             e.u = head[auxCont].id;
             e.v = SINK;
             e.cap = toSinkCap;
             head[auxCont].edgeList.push_back(e);
+
+
             auxCont++;
             scanf(" ");
         }
@@ -94,13 +115,19 @@ void scanInput() {
 
     for(i = 0; i < numLinhas; i++) {
         for(j = 0; j < numColunas - 1; j++) {
-            scanf("%d", vertexHor);
+            scanf("%d", &vertexHor);
             Edge e;
             e.u = head[auxCont].id;
             e.v = head[auxCont + 1].id;
             e.cap = vertexHor;
             head[auxCont].edgeList.push_back(e);
-            head[auxCont + 1].edgeList.push_back(e);        //ponho a aresta no u e no v ?
+
+            //adding to the vertex v, but swaping the u for v and vice versa
+            e.u = head[auxCont + 1].id;
+            e.v = head[auxCont].id;
+            e.cap = vertexHor;
+            head[auxCont + 1].edgeList.push_back(e);
+
             auxCont++;
             scanf(" ");
         }
@@ -112,19 +139,24 @@ void scanInput() {
 
     for(i = 0; i < numLinhas - 1; i++) {
         for(j = 0; j < numColunas; j++) {
-            scanf("%d", vertexVer);
+            scanf("%d", &vertexVer);
             Edge e;
             e.u = head[auxCont].id;
             e.v = head[auxCont + numColunas].id;
             e.cap = vertexVer;
             head[auxCont].edgeList.push_back(e);
-            head[auxCont + numColunas].edgeList.push_back(e);        //ponho a aresta no u e no v ?
+
+            //adding to the vertex v, but swaping the u for v and vice versa
+            e.u = head[auxCont + numColunas].id;
+            e.v = head[auxCont].id;
+            e.cap = vertexVer;
+            head[auxCont + numColunas].edgeList.push_back(e);
             auxCont++;
             scanf(" ");
         }
         scanf("\n");
     }
-
+//TODO A ligacao nao pode ser so para o actual + 1 (nas horizontais)!!!
 }
 
 
@@ -132,6 +164,9 @@ void scanInput() {
 
 int main() {
 
+    scanInput();
 
+    printf("Hello Worold\n");
+    return 0;
 
 }
